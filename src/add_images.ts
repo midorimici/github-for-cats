9ab5411f51031f7e4645583c1db08e8002ea2db3
mici <60635066+midorimici@users.noreply.github.com>
2023-07-08 18:47:27 +0900

    💄 Skip adding a image if a comment has image as its last element

--- src/add_images.ts
+++ src/add_images.ts
@@ -1,7 +1,7 @@
 import { catImageURL } from './api';
 
 export const addCatImages = async () => {
-  const comments = document.getElementsByClassName('comment-body');
+  const comments = targetComments();
   const commentCount = comments.length;
 
   const imagePromises: Promise<Element>[] = [];
@@ -14,6 +14,25 @@ export const addCatImages = async () => {
   for (let i = 0; i < commentCount; i++) {
     comments[i].appendChild(images[i]);
   }
+};
+
+const targetComments = (): Element[] => {
+  const comments = document.getElementsByClassName('comment-body');
+
+  const targets: Element[] = [];
+  for (const comment of comments) {
+    if (hasImageAsLastChild(comment)) {
+      continue;
+    }
+
+    targets.push(comment);
+  }
+
+  return targets;
+};
+
+const hasImageAsLastChild = (comment: Element): boolean => {
+  return comment.lastElementChild?.lastElementChild?.lastElementChild?.nodeName === 'IMG';
 };
 
 const createImage = async (): Promise<Element> => {
