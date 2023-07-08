14446f0cd32d9147b0d29e6deddffdba0f60ea45
mici <60635066+midorimici@users.noreply.github.com>
2023-07-08 18:22:05 +0900

    ♻️ Reorder functions in `api.ts`

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,16 @@
+export const catImageURL = async (isGIF: boolean): Promise<string> => {
+  let queryParams: string;
+  if (isGIF) {
+    queryParams = 'mime_types=gif&size=small&category_ids=1,2,4,5,7,14,15';
+  } else {
+    queryParams = 'mime_types=png,jpg';
+  }
+
+  const response = await fetchAPI(queryParams);
+  const URL = response[0].url;
+  return URL;
+};
+
 const theCatAPIURL = 'https://api.thecatapi.com/v1/images/search';
 
 type ImageData = {
@@ -13,17 +26,4 @@ const fetchAPI = async (queryParams: string): Promise<Response> => {
   const response = await fetch(`${theCatAPIURL}?${queryParams}`);
   const json = await response.json();
   return json;
-};
-
-export const catImageURL = async (isGIF: boolean): Promise<string> => {
-  let queryParams: string;
-  if (isGIF) {
-    queryParams = 'mime_types=gif&size=small';
-  } else {
-    queryParams = 'mime_types=png,jpg';
-  }
-
-  const response = await fetchAPI(queryParams);
-  const URL = response[0].url;
-  return URL;
 };
