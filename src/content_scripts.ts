b523c0a41450e96fb96fd1309ebc55c0add5c5c8
mici <60635066+midorimici@users.noreply.github.com>
2023-07-08 14:47:01 +0900

    ♻️ Use suffix constant

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -40,9 +40,12 @@ const lineEndChars = '.。!！?？';
 const lineEndRegex = `([${lineEndChars}]?)$`;
 const enLineEndRegex = new RegExp(`(\\w)${lineEndRegex}`, 'g');
 const enLineMiddleRegex = new RegExp(`(\\w)([${lineEndChars}])([ \\w])`);
+const enSuffix = 'nya';
 
 const suffixAddedText = (text: string): string => {
-  return text.replace(enLineEndRegex, '$1 nya$2').replace(enLineMiddleRegex, '$1 nya$2$3');
+  return text
+    .replace(enLineEndRegex, `$1 ${enSuffix}$2`)
+    .replace(enLineMiddleRegex, `$1 ${enSuffix}$2$3`);
 };
 
 main();
