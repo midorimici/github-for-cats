eaeb4b8e24cb1ac433b9413e42c5d87e822bad8b
mici <60635066+midorimici@users.noreply.github.com>
2023-07-08 18:21:47 +0900

    ⚡️ Use `Promise.all`

--- src/add_images.ts
+++ src/add_images.ts
@@ -2,9 +2,16 @@ import { catImageURL } from './api';
 
 export const addCatImages = async () => {
   const comments = document.getElementsByClassName('comment-body');
-  for (const comment of comments) {
-    const image = await createImage();
-    comment.appendChild(image);
+
+  const imagePromises: Promise<Element>[] = [];
+  for (let i = 0; i < comments.length; i++) {
+    const image = createImage();
+    imagePromises.push(image);
+  }
+  const images = await Promise.all(imagePromises);
+
+  for (let i = 0; i < comments.length; i++) {
+    comments[i].appendChild(images[i]);
   }
 };
 
