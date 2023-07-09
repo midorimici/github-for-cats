0417640919f71977ae873e1c97fbb93355dbca53
mici <60635066+midorimici@users.noreply.github.com>
2023-07-09 14:00:19 +0900

    ♻️ Move suffix config section to a component

---
+++ src/popup/components/SuffixConfigSection/SuffixConfigSection.tsx
@@ -0,0 +1,58 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { abSuffixKey, jaSuffixKey, useFetchSuffix } from './useFetchSuffix';
+import { useSaveSuffix } from './useSaveSuffix';
+
+const t = chrome.i18n.getMessage;
+
+export const SuffixConfigSection: React.FC = () => {
+  const suffixLabel = t('suffix');
+  const abLabel = t('alphabets');
+  const jaLabel = t('japanese');
+
+  const [abSuffix, setAbSuffix] = useState<string>('');
+  const [jaSuffix, setJaSuffix] = useState<string>('');
+  useFetchSuffix(setAbSuffix, setJaSuffix);
+
+  return (
+    <section className="suffix-config-container">
+      {suffixLabel}
+      <SuffixForm
+        label={abLabel}
+        suffix={abSuffix}
+        setSuffix={setAbSuffix}
+        suffixKey={abSuffixKey}
+      />
+      <SuffixForm
+        label={jaLabel}
+        suffix={jaSuffix}
+        setSuffix={setJaSuffix}
+        suffixKey={jaSuffixKey}
+      />
+    </section>
+  );
+};
+
+type SuffixFormProps = {
+  label: string;
+  suffix: string;
+  setSuffix: (suffix: string) => void;
+  suffixKey: string;
+};
+
+const SuffixForm: React.FC<SuffixFormProps> = ({ label, suffix, setSuffix, suffixKey }) => {
+  const { handleBlur, isShowingDoneIcon } = useSaveSuffix(suffix, setSuffix, suffixKey);
+
+  return (
+    <label className="label">
+      {label}
+      <div className="inline-form">
+        <input type="text" defaultValue={suffix} onBlur={handleBlur} />
+        <div className="icon-container">
+          {isShowingDoneIcon && <FontAwesomeIcon icon={faCheck} />}
+        </div>
+      </div>
+    </label>
+  );
+};
