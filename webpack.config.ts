52e8cc24cf126913efbb9928fc4a2697bfe13eee
mici <60635066+midorimici@users.noreply.github.com>
2023-07-09 10:25:34 +0900

    🔧 Setup for Sass

--- webpack.config.ts
+++ webpack.config.ts
@@ -21,6 +21,21 @@ const config = (): Configuration => {
           use: 'ts-loader',
           exclude: '/node_modules/',
         },
+        {
+          exclude: /node_modules/,
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader', // Creates style nodes from JS strings
+            },
+            {
+              loader: 'css-loader', // Translates CSS into CommonJS
+            },
+            {
+              loader: 'sass-loader', // Compiles Sass to CSS
+            },
+          ],
+        },
       ],
     },
     resolve: {
