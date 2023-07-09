b0b86e91e0aba680344f40098c25ac78d2849a10
mici <60635066+midorimici@users.noreply.github.com>
2023-07-09 11:52:36 +0900

    ✨ Save suffix config to storage

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,19 +1,26 @@
 import { useSuffix } from './useSuffix';
 import './popup.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const t = chrome.i18n.getMessage;
 
 export const Popup = () => {
   const configLabel = t('configurations');
   const suffixLabel = t('suffix');
-  const { suffix } = useSuffix();
+  const { suffix, handleBlur, isShowingDoneIcon } = useSuffix();
 
   return (
     <div>
       <h1 className="title">{configLabel}</h1>
       <label className="label">
         {suffixLabel}
-        <input type="text" defaultValue={suffix} />
+        <div className="inline-form">
+          <input type="text" defaultValue={suffix} onBlur={handleBlur} />
+          <div className="icon-container">
+            {isShowingDoneIcon && <FontAwesomeIcon icon={faCheck} />}
+          </div>
+        </div>
       </label>
     </div>
   );
--- src/popup/useSuffix.ts
+++ src/popup/useSuffix.ts
@@ -1,16 +1,20 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, type FocusEventHandler, type FocusEvent } from 'react';
 
 const storage = chrome.storage.local;
+const suffixKey = 'suffix';
 
 type UseSuffixReturnType = {
   suffix: string;
+  handleBlur: FocusEventHandler<HTMLInputElement>;
+  isShowingDoneIcon: boolean;
 };
 
 export const useSuffix = (): UseSuffixReturnType => {
   const [suffix, setSuffix] = useState<string>('');
+  const [isShowingDoneIcon, setIsShowingDoneIcon] = useState<boolean>(false);
 
   const fetch = useCallback(() => {
-    storage.get({ suffix: chrome.i18n.getMessage('defaultSuffix') }).then((val) => {
+    storage.get({ [suffixKey]: chrome.i18n.getMessage('defaultSuffix') }).then((val) => {
       setSuffix(val.suffix);
     });
   }, []);
@@ -19,5 +23,23 @@ export const useSuffix = (): UseSuffixReturnType => {
     fetch();
   }, [fetch]);
 
-  return { suffix };
+  const handleBlur: FocusEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      const value = e.target.value;
+      if (value === suffix) {
+        return;
+      }
+
+      setSuffix(value);
+      storage.set({ [suffixKey]: value });
+
+      setIsShowingDoneIcon(true);
+      setTimeout(() => {
+        setIsShowingDoneIcon(false);
+      }, 3000);
+    },
+    [suffix, setSuffix]
+  );
+
+  return { suffix, handleBlur, isShowingDoneIcon };
 };
