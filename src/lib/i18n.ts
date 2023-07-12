cbac11e7b9aaac5660478cef9bd2ae84a475d3cf
mici <60635066+midorimici@users.noreply.github.com>
2023-07-12 23:40:14 +0900

    💬 Add description

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,5 +1,5 @@
 import data from '../../public/_locales/ja/messages.json';
 
-export const t = (key: keyof typeof data): string => {
-  return chrome.i18n.getMessage(key);
+export const t = (key: keyof typeof data, args?: string[]): string => {
+  return chrome.i18n.getMessage(key, args);
 };
--- src/options/components/domain/SkipPagesSection/SkipPagesSection.tsx
+++ src/options/components/domain/SkipPagesSection/SkipPagesSection.tsx
@@ -1,3 +1,4 @@
+import './styles.scss';
 import { t } from '~/lib/i18n';
 import { List } from '~/options/components/shared/List';
 import { useList } from '~/options/components/domain/hooks/useList';
@@ -12,6 +13,7 @@ export const SkipPagesSection: React.FC = () => {
     <section className="config-container">
       <label className="label">
         {skipPagesLabel}
+        <Description />
         <div className="inline-form">
           <input
             type="text"
@@ -24,4 +26,38 @@ export const SkipPagesSection: React.FC = () => {
       <List list={pages} deleteItem={deletePage} />
     </section>
   );
+};
+
+const Description: React.FC = () => {
+  const skipPagesDescription = t('skipPagesDesc', [
+    '^https://github.com/',
+    '.*settings',
+    '^https://github.com/.*settings',
+  ]);
+  const [desc1, desc2] = skipPagesDescription.split('\n');
+
+  return (
+    <>
+      <p className="description">{convertToElement(desc1)}</p>
+      <p className="description">{convertToElement(desc2)}</p>
+    </>
+  );
+};
+
+const convertToElement = (text: string): JSX.Element => {
+  const elements: JSX.Element[] = [];
+
+  const matches = text.matchAll(/(.*?)<(.*?)>([^<]*)/g);
+  for (const match of matches) {
+    const element: JSX.Element = (
+      <>
+        {match[1]}
+        <code>{match[2]}</code>
+        {match[3]}
+      </>
+    );
+    elements.push(element);
+  }
+
+  return <>{elements}</>;
 };
