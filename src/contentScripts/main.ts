bd3796d4384207951815c11b0320c873dd4680f7
mici <60635066+midorimici@users.noreply.github.com>
2023-07-12 21:55:06 +0900

    ✨ Skip conversion in ignored pages

--- src/contentScripts/main.ts
+++ src/contentScripts/main.ts
@@ -1,15 +1,23 @@
 import { addCatImages } from './addImages';
 import { addSuffixes } from './addSuffixes';
-import { fetchFromStorage } from '../lib/storage';
 import { replaceAvatarImages } from './replaceAvatarImages';
+import { fetchFromStorage } from '~/lib/storage';
+
+let isSkippedPage: boolean;
 
 const main = async () => {
+  isSkippedPage = await shouldSkip();
+  if (isSkippedPage) {
+    return;
+  }
+
   const { isSuffixEnabled, isAvatarImageReplacementEnabled, isAddImagesToCommentsEnabled } =
     await fetchFromStorage([
       'isSuffixEnabled',
       'isAvatarImageReplacementEnabled',
       'isAddImagesToCommentsEnabled',
     ]);
+
   if (isSuffixEnabled) {
     addSuffixes();
   }
@@ -21,6 +29,21 @@ const main = async () => {
   if (isAddImagesToCommentsEnabled) {
     addCatImages();
   }
+};
+
+const shouldSkip = async (): Promise<boolean> => {
+  const currentURL = location.href;
+
+  const { skipPages } = await fetchFromStorage(['skipPages']);
+
+  for (const pattern of skipPages) {
+    const regex = new RegExp(`^https://github.com/${pattern}`);
+    if (regex.test(currentURL)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 const setupObserver = () => {
@@ -45,9 +68,13 @@ setupObserver();
 chrome.runtime.onMessage.addListener(
   (msg: 'refreshImages', _, sendResponse: (response: boolean) => void) => {
     if (msg === 'refreshImages') {
-      replaceAvatarImages();
+      if (!isSkippedPage) {
+        replaceAvatarImages();
+      }
       sendResponse(true);
+      return;
     }
+
     sendResponse(false);
   }
 );
